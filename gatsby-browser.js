aac33c59bdc945e800ec6fd0217e1db6a7b95c53
Jeff Hackshaw <jeffrey.hackshaw@gmail.com>
2020-06-29 10:23:00 -0400

    sharing python code in docker post

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,10 @@ import React from "react";
 import { MDXProvider } from "@mdx-js/react";
 import { CodeBlock } from "./src/components";
 
+import Prism from "prism-react-renderer/prism";
+(typeof global !== "undefined" ? global : window).Prism = Prism;
+require("prismjs/components/prism-docker");
+
 const components = {
   code: CodeBlock
 };
